0a553aaaea158454d526577b16eb81b521736f55
Vitalii Soloninko <130390124+VitaliiSoloninko@users.noreply.github.com>
2025-11-24 17:16:24 +0100

    fix(home page): fix link

--- src/app/pages/home-page/hero-section/hero-section.component.ts
+++ src/app/pages/home-page/hero-section/hero-section.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-hero-section',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './hero-section.component.html',
-  styleUrl: './hero-section.component.scss'
+  styleUrl: './hero-section.component.scss',
 })
-export class HeroSectionComponent {
-
-}
+export class HeroSectionComponent {}
