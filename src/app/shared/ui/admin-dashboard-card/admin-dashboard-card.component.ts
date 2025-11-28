aac0b32b24db44bbb192e0c19e4956c00fb8bb11
Vitalii Soloninko <130390124+VitaliiSoloninko@users.noreply.github.com>
2025-11-28 23:24:59 +0100

    feat(admin dashboard): made better layout

--- src/app/shared/ui/admin-dashboard-card/admin-dashboard-card.component.ts
+++ src/app/shared/ui/admin-dashboard-card/admin-dashboard-card.component.ts
@@ -10,7 +10,6 @@ export class AdminDashboardCardComponent {
   title = input<string>('');
   value = input<string | number>('');
   icon = input<string>();
-  trend = input<{ value: number; isPositive: boolean }>();
   color = input<string>('#3b82f6');
 
   Math = Math;
