0a3b27e8cec6458654a35d5f01a3788ea8189958
Vitalii Soloninko <130390124+VitaliiSoloninko@users.noreply.github.com>
2025-11-29 01:18:32 +0100

    feat(admin dashboard): add types count

--- src/app/pages/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/pages/admin/dashboard-page/dashboard-page.component.ts
@@ -1,10 +1,10 @@
-import { Component, effect, signal } from '@angular/core';
+import { Component, effect, inject, signal } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { ORDERS } from '../../../data/orders.data';
 import { PRODUCTS_DATA } from '../../../data/products.data';
-import { TYPES } from '../../../data/types.data';
 import { USERS } from '../../../data/users.data';
 import { BrandService } from '../../../entities/brand/api/brand.service';
+import { TypeService } from '../../../entities/type/api/type.service';
 import { AdminDashboardCardComponent } from '../../../shared/ui/admin-dashboard-card/admin-dashboard-card.component';
 import { GrayLineComponent } from '../../../shared/ui/gray-line/gray-line.component';
 
@@ -18,21 +18,33 @@ export class DashboardPageComponent {
   stats = signal({
     totalProducts: PRODUCTS_DATA.rows.length,
     totalBrands: 0,
-    totalTypes: TYPES.length,
+    totalTypes: 0,
     totalUsers: USERS.length,
     totalOrders: ORDERS.length,
     totalRevenue: this.calculateTotalRevenue(),
   });
 
-  totalBrands;
+  private brandService = inject(BrandService);
+  private typeService = inject(TypeService);
 
-  constructor(private brandService: BrandService) {
+  totalBrands;
+  totalTypes;
+
+  constructor() {
     this.totalBrands = toSignal(this.brandService.getBrandsCount(), {
       initialValue: 0,
     });
     effect(() => {
       const count: number = this.totalBrands();
       this.stats.update((s) => ({ ...s, totalBrands: count }));
+    });
+
+    this.totalTypes = toSignal(this.typeService.getTypesCount(), {
+      initialValue: 0,
+    });
+    effect(() => {
+      const count: number = this.totalTypes();
+      this.stats.update((s) => ({ ...s, totalTypes: count }));
     });
   }
 
